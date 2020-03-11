b00aec9338c1fc92da649e13f23125ca38c548ac
klinga <klingaroo@gmail.com>
2020-03-11 09:19:53 -0400

    improved ingest

--- bagel/ingest.py
+++ bagel/ingest.py
@@ -25,6 +25,49 @@ Row = namedtuple('Row', [
     'email'])
 
 
+def lower_first_letter(string):
+    try:
+        string = string.strip()
+        string = f'{string[0].lower()}{string[1:]}'
+    except IndexError:
+        string = string
+    except AttributeError:
+        string = str(string)
+    return string
+
+
+def has_alphanumeric_last(string=None):
+    try:
+        return string[-1].isalnum()
+    except AttributeError:
+        raise
+    except TypeError:
+        raise
+    except IndexError:
+        raise
+
+def remove_left_white_space(string=None):
+    try:
+        return string.lstrip()
+    except AttributeError:
+        raise
+
+
+def remove_white_space_and_trailing_punctuation(string):
+    if string:
+        string = remove_left_white_space(string)
+        while has_alphanumeric_last(string) is False:
+            string = string[:-1]
+    else:
+        string = ''
+
+    return string
+
+
+def str2list(string):
+    return [i.strip() for i in string.split(';') if i.strip() != '']
+
+
 def form_data_reader(fh):
     """
     Parses descripions from a csv file created from google sheet
@@ -42,12 +85,15 @@ def form_data_reader(fh):
 
         for row in reader:
             # add regex validation?
-            central_barcodes = [b.strip() for b in row[7].split(';') if b.strip() != '']
-            crown_barcodes = [b.strip() for b in row[8].split(';') if b.strip() != '']
-            title_other = [t.strip() for t in row[10].split(';') if t.strip() != '']
-            isbn = [i.strip() for i in row[13].split(';') if i.strip() != '']
-            upc = [u.strip() for u in row[14].split(';') if u.strip() != '']
-            title = row[2].strip()  # run regex to remove trailing periods
+            central_barcodes = str2list(row[7])
+            crown_barcodes = str2list(row[8])
+            title_other = str2list(row[10])
+            isbn = str2list(row[13])
+            upc = str2list(row[14])
+            title = remove_white_space_and_trailing_punctuation(row[2])  # run regex to remove trailing periods
+            title_other = lower_first_letter(row[10])
+            desc = remove_white_space_and_trailing_punctuation(row[18])
+            content = remove_white_space_and_trailing_punctuation(row[19])
 
             yield Row(
                 processing=row[1],
@@ -67,6 +113,6 @@ def form_data_reader(fh):
                 pub_place=row[15].strip(),
                 publisher=row[16].strip(),
                 pub_date=row[17].strip(),  # validation?
-                desc=row[18].strip(),
+                desc=desc,
                 content=row[19].strip(),
                 email=row[20])
--- tests/context.py
+++ tests/context.py
@@ -6,4 +6,8 @@ sys.path.insert(0, p + '\\' + p.split('\\')[-1])
 sys.path.insert(0, p)
 
 
-from bagel.ingest import form_data_reader
+from bagel.ingest import (
+    form_data_reader, lower_first_letter, has_alphanumeric_last,
+    remove_white_space_and_trailing_punctuation, str2list,
+    remove_left_white_space, str2list
+)
